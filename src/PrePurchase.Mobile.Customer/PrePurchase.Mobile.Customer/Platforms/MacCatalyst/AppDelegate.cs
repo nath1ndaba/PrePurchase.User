7c782935a1e3744740edaada85301f920ab1059e
nath1ndaba <nath1ndaba@outlook.com>
2026-01-04 18:54:19 +0200

    Customer initialised

---
+++ src/PrePurchase.Mobile.Customer/PrePurchase.Mobile.Customer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PrePurchase.Mobile.Customer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
